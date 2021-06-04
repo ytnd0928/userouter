8afdbb37deffd73702774ab9a1c33ef4d469c80e
nodachan <72251931+ytnd0928@users.noreply.github.com>
2021-06-04 21:29:37 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+
+import { BrowserRouter, Link } from "react-router-dom";
 
 import { Router } from "./router.Router";
-import {Page1} from "./Page1";
-import {Page1DetailA} from "./Page1DetailA";
-import {Page1DetailB} from "./Page1DetailB";
-import { Page2 } from "./Page2";
+
 import { Home } from "./Home";
 
+import { Page1 } from "./Page1";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
+import { Page2 } from "./Page2";
 
 import "./styles.css";
 
@@ -22,32 +24,6 @@ export default function App() {
         <br />
       </div>
       <Router />
-
-      <Switch>
-      <Route exact path="/">
-           <Home />
-         </Route>
-         <Route 
-         path="/page1"
-         render={({ match: { url }}) => (
-           <Switch>
-             <Page1 />
-           </Switch>
-         )
-         } />
-           <Page1 />
-         </Route>
-         <Route path="/page1/detailA">
-           <Page1DetailA />
-         </Route>
-         <Route path="/page1/detailB">
-           <Page1DetailB />
-         </Route>
-         </Switch>
-         <Route path="/page2">
-           <Page2/>
-         </Route>
-      </Switch>
-   </BrowserRouter>
+    </BrowserRouter>
   );
 }
